39739b5654d7608c693fc52773aa1490c31887f7
sspolash <sspolash@gmail.com>
2021-01-15 13:09:16 +0600

    I am changing something of all files

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let firstName = "Md. Fazlur Rahman"
 let lastName = "Polash"
-let fullName = firstName + lastName
+let fullName = firstName + " " + lastName
 console.log(fullName)
+
+let age = 25
+
+console.log("My name is " + fullName + "." + " I am " + age + " years old." )
